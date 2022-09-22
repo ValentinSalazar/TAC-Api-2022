26fcb4fe522c603653cac78c721f91b35906acdc
Facundo Desktop <facundev@outlook.com>
2022-09-22 18:06:32 -0300

    Terminado de Creacion de Usuario correctamente. Falta terminar el Login.

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,48 +1,57 @@
-const express = require('express')
-const userSchema = require('../models/userSchema.js')
-const routerUser = express.Router()
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const userSchema = require("../models/userSchema.js");
+const routerUser = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { Code } = require("mongodb");
+require("dotenv").config();
 
-routerUser.post('/user/create', async (req, res) => {
-    try {
-        console.log('Endpoint userCreate funcionando.');
+routerUser.post("/user/create", async (req, res) => {
+  try {
+    console.log("- Endpoint userCreate funcionando.");
+    const { name, email, password } = req.body;
 
-        // const userFound = userSchema.find(email: )
-        const { name, email, password } = req.body
-        const passwordHash = await bcrypt.hash(password, 8)
-        const newUser = new userSchema({
-            name,
-            email,
-            password: passwordHash
-        })
+    const userFound = await userSchema.find({ email });
+    const validarEmail = Object.keys(userFound).length !== 0;
+    if (validarEmail) {
+      return res.status(409).json({ error: "El mail ya se encuentra registrado." });
+    } else {
+      const passwordHash = await bcrypt.hash(password, 8);
+      const newUser = new userSchema({
+        name,
+        email,
+        password: passwordHash,
+      });
 
-        const savedUser = await newUser.save()
+      const savedUser = await newUser.save();
+      console.log(savedUser);
 
-        const token = jwt.sign({ id: savedUser._id }, 'institucionales-api', {
-            expiresIn: 43200
-        })
-        res.json({ token })
-    } catch (err) {
-        console.log(err);
+      return res.status(201).json({mensaje: 'Se ha registrado correctamente.'})
     }
-})
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-routerUser.post('/user/login', async (req, res) => {
-    try {
-        const userFound = await userSchema.findOne({ email: req.body.email })
+routerUser.post("/user/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-        if (req.body.password !== userFound.password) {
-            console.log('asd');
-            //return res.status(401).json({token: null, message: 'Password invalida.'})
-            res.status(401).json({ token: null, message: 'password invalida.' })
-        } else {
-            res.json({ token: 'logueado' })
-        }
-    } catch (err){
-        console.log(err);
+    // const userFound = await userSchema.findOne({ email: req.body.email });
+
+    const user = await userSchema.findOne({ email });
+    console.log(user);
+    const passwordCorrect =
+      user === null ? false : await bcrypt.compare(password, user.password);
+
+    if (!passwordCorrect) {
+      res.status(401).json({
+        error: "Usuario o contrasenia invalido.",
+      });
     }
-})
-
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = routerUser;
